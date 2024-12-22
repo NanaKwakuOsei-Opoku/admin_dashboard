28d9629b8ef0feb74ca1fe8804393f4a302c93d0
NanaKwakuOsei-Opoku <95250194+NanaKwakuOsei-Opoku@users.noreply.github.com>
2024-12-22 21:02:13 +0000

    Updated layout and added LeftSidebar component rendering

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Guitar Hub - Admin Auth",
+  description: "Admin dashboard to manage Guitar Hub data",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
